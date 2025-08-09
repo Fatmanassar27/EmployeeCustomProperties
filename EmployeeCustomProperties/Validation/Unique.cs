af2d3c3a10f2b6252d270db4f0a4b3774d7fc4f3
Fatma Nassar <Fatma.nassar27@outlook.com>
2025-08-09 23:54:54 +0300

    Add edit employee view

--- EmployeeCustomProperties/Validation/Unique.cs
+++ EmployeeCustomProperties/Validation/Unique.cs
@@ -1,25 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using EmployeeCustomProperties.Data;
 
-namespace EmployeeCustomProperties.Validation
+public class Unique : ValidationAttribute
 {
-    public class Unique : ValidationAttribute
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            var context = (DBContext)validationContext.GetService(typeof(DBContext));
+        var context = (DBContext)validationContext.GetService(typeof(DBContext));
 
-            var code = value?.ToString();
+        var code = value?.ToString();
 
-            if (string.IsNullOrWhiteSpace(code))
-                return ValidationResult.Success;
-
-            var exists = context.Employees.Any(e => e.Code == code);
-
-            if (exists)
-                return new ValidationResult("This employee code already exists.");
-
+        if (string.IsNullOrWhiteSpace(code))
             return ValidationResult.Success;
-        }
+        var instance = validationContext.ObjectInstance;
+        var employee = instance as EmployeeCustomProperties.Models.Employee;
+        var currentEmployeeId = employee?.Id ?? 0;
+
+        var exists = context.Employees.Any(e => e.Code == code && e.Id != currentEmployeeId);
+
+        if (exists)
+            return new ValidationResult("This employee code already exists.");
+
+        return ValidationResult.Success;
     }
 }
